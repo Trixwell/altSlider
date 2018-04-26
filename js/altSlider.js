1f7fd2c37d7f4abbb8e6bb481077a95084343ba7
Firebaba <ntbilieli@gmail.com>
2018-04-26 20:24:00 +0300

    Add support for scroll and fix left and right issues. Rename "examples" to "try-it".

--- js/altSlider.js
+++ js/altSlider.js
@@ -13,24 +13,31 @@ $.fn.altSlider = function (userConfig) {
         slider[0].addEventListener('wheel', function (e) {
             let item_width = $(this).find('.item').width();
             let total_width = $(this).width();
-            let wrapper_width = $(this).find('.scroll_wrapper').width();
+            let wrapper_width = $(this).parent().find('.scroll_wrapper').width();
             let scroll_bar_width = $(this).find('.scroll_bar').width();
             let item_length = $(this).find('.item').length;
-
-
-            console.log(item_width / total_width);
+            let left_side = wrapper_width / item_length;
+            let scrollbar_left_size = parseInt($(this).parent().find('.scroll_bar').css('left'));
 
             if (e.deltaY < 0) {
                 item_width *= -1;
+                left_side *= -1;
             }
 
-            let scroll_bar_left = $(this).find('.scroll_bar').css('left', '');
+            let left_size_bar = scrollbar_left_size + left_side;
+            if (left_size_bar < 0) {
+                left_size_bar = 0;
+            }
+
+            if (left_size_bar > (total_width - left_side)) {
+                left_size_bar = total_width - left_side;
+            }
+
+            $(this).parent().find('.scroll_bar')
+                .css('left', left_size_bar);
 
             this.scrollLeft = this.scrollLeft + item_width;
         });
-
-
-        // SCROLLBAR CUSTOMIZATION
 
         let scroll_wrapper = $('<div />')
             .addClass('scroll_wrapper');
@@ -38,22 +45,17 @@ $.fn.altSlider = function (userConfig) {
         let scroll_bar = $('<div />')
             .addClass('scroll_bar');
 
-        slider.append(scroll_wrapper);
+        $(this).append(scroll_wrapper);
         scroll_wrapper.append(scroll_bar);
 
-
-
-
-
-
-        ///////////////////////////////////////
         $.ajax({
             url: config.url,
             type: 'post',
             dataType: 'json',
             success: function (res) {
-                let wrapper_width = $(slider).find('.scroll_wrapper').width();
+                let wrapper_width = $(slider).parent().find('.scroll_wrapper').width();
                 let elem_width = wrapper_width / res.length;
+
                 $(scroll_bar).css('width', elem_width + 'px');
 
                 res.forEach(function (el) {
---
+++ try-it/test1/index.js
@@ -0,0 +1,8 @@
+$(function () {
+    $(".block").altSlider(
+        {
+            url: '/try-it/test1/test.json'
+        }
+    );
+});
+
